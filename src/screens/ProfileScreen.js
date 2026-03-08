08ab8c5fee895a9cba1ff9703adc1c6e48e43830
Fararaza87 <fararaza87@gmail.com>
2026-03-08 11:21:52 +0100

    Create ProfileScreen component

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProfileScreen = () => {
+    return (
+        <div>
+            <h1>User Profile</h1>
+            <p>Name: [User Name]</p>
+            <p>Email: [User Email]</p>
+            <p>Bio: [User Bio]</p>
+        </div>
+    );
+};
+
+export default ProfileScreen;
